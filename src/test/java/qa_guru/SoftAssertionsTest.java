7312bd2cf491fbcd4194ad6c43ddbfc181c29ad8
Julia Malkova <yuliamalkova1606@gmail.com>
2023-07-25 09:54:44 +0300

    Removed unused import and configuration lines

--- src/test/java/qa_guru/SoftAssertionsTest.java
+++ src/test/java/qa_guru/SoftAssertionsTest.java
@@ -1,22 +1,16 @@
 package qa_guru;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SoftAssertionsTest extends TestData {
-    static {
-        ChromeOptions options = new ChromeOptions();
-        options.setBinary("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        Configuration.browserCapabilities = capabilities;
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
     }
 
     @Test
